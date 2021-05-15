85cdbac9065e2b8e2e52306427eff6b4dcff8ecb
Stephen Pirpinias <apudpirpis@gmail.com>
2021-05-15 14:35:28 -0400

    Create contactHMDB.py

---
+++ contactHMDB.py
@@ -0,0 +1,17 @@
+## This is a program that I wrote in the Laboratory of Jan Krumsiek for converting Metabolites into HMDI.
+## I used a web scraper to submit a form to the website.
+
+from selenium import webdriver
+import pandas as pd
+
+#Initiate the Driver
+browser=webdriver.Chrome()
+browser.get('https://www.metaboanalyst.ca/faces/upload/ConvertView.xhtml')
+
+#Send the Driver to Submit the Form
+chooseFile=browser.find_element_by_id('form1:j_idt48').send_keys("/home/spirpinias/Desktop/myTest.dat")
+submitFile=browser.find_element_by_id('form1:j_idt61').click()
+elems=browser.find_element_by_xpath("//*[@id='form']/table[2]/tbody/tr[2]/td/a").get_attribute("href")
+    
+#Download the CSV and Create Table
+listofIDS=pd.read_csv(elems)
