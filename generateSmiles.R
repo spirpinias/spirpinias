3cc0ea85a8ff25cf34ba4f5ab7535eeaa6e60330
Stephen Pirpinias <apudpirpis@gmail.com>
2021-05-15 14:32:04 -0400

    Update generateSmiles.R

--- generateSmiles.R
+++ generateSmiles.R
@@ -1,6 +1,6 @@
 ## This is a program that I wrote for converting metabolites into SMILES. 
 ## I was trying to find another way to study the problem. The first hurdle was the convert the Metabolites to Smiles.
-## This 
+## You can download these 2 files from https://figshare.com/articles/dataset/Qatar_Metabolomics_Study_on_Diabetes/5904022?file=10531345
 
 library(purrr)
 library(readxl)
@@ -26,13 +26,11 @@ cleanMetabolites=gsub("[(][^)]*[)]","",cleanMetabolites)
 #Remove String after last comma
 cleanMetabolites=sub('^([^,]+,[^,]+).*', '\\1', cleanMetabolites)
 
-
 #Remove String in Brackets
 cleanMetabolites=sub("\\[.*]", "", cleanMetabolites)
 
 #Delete the whitespace
 cleanMetabolites=gsub(" ", "", cleanMetabolites, fixed = TRUE)
-
 
 #Remove trailing Asterisk
 cleanMetabolites=gsub("[*].*$","",cleanMetabolites)
@@ -42,6 +40,6 @@ generateSMILES=function(j){
   url=download.file(url = paste0('http://opsin.ch.cam.ac.uk/opsin/',cleanMetabolites[j],".smi"),destfile = paste0(cleanMetabolites[j],'.smi'))
 }
 
-#Error Exception Handling
+#Error Exception Handling to continue if OPSIN does not have information on your metabolite.
 possiblySomeSMILES=possibly(generateSMILES,otherwise = "No Information")
 possiblySomeInfo=map(1:length(cleanMetabolites),possiblySomeSMILES)
